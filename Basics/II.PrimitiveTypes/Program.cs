341e153fe82d0f1ea6cc01274e7510d8e2858649
Private <87845569+nikx-gupta@users.noreply.github.com>
2022-07-03 15:02:06 +0530

    update

--- Basics/II.PrimitiveTypes/Program.cs
+++ Basics/II.PrimitiveTypes/Program.cs
@@ -1,3 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using static System.Console;
 
-Console.WriteLine("Hello, World!");
+namespace II.PrimitiveTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Conversion();
+        }
+
+        public static void Conversion()
+        {
+            // Without Validation
+            // Throws FormatErrorException when Conversion not Possible
+            int x = Convert.ToInt32("34");
+
+            try
+            {
+                Convert.ToInt32("45.67");
+            }
+            catch (FormatException ex)
+            {
+                WriteLine(ex.Message);
+            }
+            
+            // With Validation
+            // Returns True if Conversion is Possible
+            if (int.TryParse("34.56", out int strToInt))
+            {
+                
+            }
+        }
+        
+        public static void PrimitiveTypesSize()
+        {
+            WriteLine("--------------------------------------------------------------------------");
+            WriteLine("Type    Byte(s) of memory               Min                            Max");
+            WriteLine("--------------------------------------------------------------------------");
+            WriteLine($"sbyte   {sizeof(sbyte),-4} {sbyte.MinValue,30} {sbyte.MaxValue,30}");
+            WriteLine($"byte    {sizeof(byte),-4} {byte.MinValue,30} {byte.MaxValue,30}");
+            WriteLine($"short   {sizeof(short),-4} {short.MinValue,30} {short.MaxValue,30}");
+            WriteLine($"ushort  {sizeof(ushort),-4} {ushort.MinValue,30} {ushort.MaxValue,30}");
+            WriteLine($"int     {sizeof(int),-4} {int.MinValue,30} {int.MaxValue,30}");
+            WriteLine($"uint    {sizeof(uint),-4} {uint.MinValue,30} {uint.MaxValue,30}");
+            WriteLine($"long    {sizeof(long),-4} {long.MinValue,30} {long.MaxValue,30}");
+            WriteLine($"ulong   {sizeof(ulong),-4} {ulong.MinValue,30} {ulong.MaxValue,30}");
+            WriteLine($"float   {sizeof(float),-4} {float.MinValue,30} {float.MaxValue,30}");
+            WriteLine($"double  {sizeof(double),-4} {double.MinValue,30} {double.MaxValue,30}");
+            WriteLine($"decimal {sizeof(decimal),-4} {decimal.MinValue,30} {decimal.MaxValue,30}");
+            WriteLine("--------------------------------------------------------------------------");
+        }
+    }
+}
