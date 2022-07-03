c8d6be9143bc0ad7029989c84ebf4ac2bef89442
Private <87845569+nikx-gupta@users.noreply.github.com>
2022-07-03 14:51:42 +0530

    Add basics

---
+++ Basics/I.HelloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace I.HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");            
+        }
+    }
+}
